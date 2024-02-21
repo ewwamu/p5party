a324894d9c4a07d548f908080b262e3dc66322d5
emma wu <wue375@newschool.edu>
2024-02-21 02:07:20 -0500

    plsworkprettypls

--- p5.party-main/5x5Fill/sketch.js
+++ p5.party-main/5x5Fill/sketch.js
@@ -18,7 +18,10 @@ let sceneNum = 0;
 // tracking if game is running, keeping it at t/f 'logic gate' to control app flow
 let gameRunningState = false;
 
-let testImg;
+// players
+let lychee;
+let kumquat;
+let durian;
 
 let imgs = {
 	'tomato': undefined,
@@ -26,7 +29,7 @@ let imgs = {
 }
 
 // Color palette
-const palette = ['tomato', 'darkorange', 'gold', 'chartreuse', 'aqua'];
+const palette = ['tomato', 'darkorange', 'chartreuse'];
 
 // Load the sketch; define variables
 function preload() {
@@ -46,7 +49,11 @@ function preload() {
 	// imgs.tomato = loadImage('tomatoImage');
 	// imgs.darkorange = loadImage('orangeImage');
 
-	testImg = loadImage('https://picsum.photos/id/19/200/200')
+	lychee = loadImage('./assets/lychee.png')
+	kumquat = loadImage('./assets/kumquat.png')
+	durian = loadImage('./assets/durian.png')
+
+
 }
 
 
@@ -56,7 +63,11 @@ function setup() {
 	// createCanvas(400, 500);
 	//  Host has to click start for the timer to begin
 	button = createButton('start');
-	button.position(250, 0);
+	button.position(CENTER);
+	button.style('font-size', '20px');
+	button.style('background-color', 'hotpink');
+	button.style('border-radius', '25px');
+
 	
 	ellipseMode(CORNER);
 	console.log('init setup done')
@@ -66,10 +77,12 @@ function setup() {
 		// Initialize grid; check if there is a host instead of shared grid
 		resetGrid();
 		// Only one person has timer (host) who'll control
-		shared_time.gameTimer = 15; // Change this to the desired initial value
+		shared_time.gameTimer = 20; // Change this to the desired initial value
 		// Only one person has timer (host) who'll control
 		partySetShared(shared_time, { gameTimer: shared_time.gameTimer });
 	}
+	textFont('FT88-Regular');
+
 }
 
 // Defines result of where you click on the grid
@@ -79,21 +92,21 @@ function setup() {
 function mousePressed() {
 	const p1 = guests.find((p) => p.role === 'tomato');
 	const p2 = guests.find((p) => p.role === 'darkorange');
-	const p3 = guests.find((p) => p.role === 'gold');
-	const p4 = guests.find((p) => p.role === 'chartreuse');
-	const p5 = guests.find((p) => p.role === 'aqua');
+	const p3 = guests.find((p) => p.role === 'chartreuse');
+	// const p4 = guests.find((p) => p.role === 'chartreuse');
+	// const p5 = guests.find((p) => p.role === 'aqua');
 	
 	// where switch on happ! true
 	gameRunningState = true;
 	console.log("gameState made true!")
-	if (p1 === me || p2 === me || p3 === me || p4 === me || p5 === me) {
+	if (p1 === me || p2 === me || p3 === me) {
 		drawColor = me.role;
 		// 5 boxes by 5 boxes
-		let col = floor(mouseX / 80);
-		let row = floor(mouseY / 80);
+		let col = floor(mouseX / 100);
+		let row = floor(mouseY / 100);
 
 		// Palette is the bottom row, each column gets a color
-		if (col >= 0 && col <= 4 && row >= 0 && row <= 5) {
+		if (col >= 0 && col <= 5 && row >= 0 && row <= 5) {
 			if (shared.grid[col][row] === drawColor) {
 				shared.grid[col][row] = false;
 			} else {
@@ -119,14 +132,17 @@ function draw() {
 	// important loop one
 	if (sceneNum == 0) {
 		// sends 0 --> 1 
+		createCanvas(windowWidth, windowHeight);
 		button.mousePressed(updatesceneNum);
 		text('game instructions', 0, 50);
+		background('lightpink');
+
 	}
 	console.log(gameRunningState)
 	// loop two (main, "always" on)
 	if (sceneNum == 1 && gameRunningState === true) {
-		createCanvas(400, 500);
-		background('lightgray');
+		createCanvas(windowWidth, windowHeight);
+		background('papayawhip');
 		assignPlayers();
 		drawPixels();
 		gameTimer();
@@ -142,7 +158,7 @@ function updatesceneNum() {
 // Defines color palette
 function drawUI() {
 	// push() pop() allows you to switch between colors
-	push();
+	// push();
 	strokeWeight(3);
 
 	// When clicked, color selected and palette stroke changes
@@ -153,13 +169,14 @@ function drawUI() {
 		} else {
 			stroke('black');
 		}
-		ellipse(i * 80 + 4, 400 + 4, 80 - 8, 80 - 8);
+		ellipse(i * 100 + 104, 500 + 4, 100 - 8, 100 - 8);
 	}
-	pop();
+	// pop();
 
 	// Display timer
-	textFont('Times New');
-	text(shared_time.gameTimer, 10, 60);
+	textFont('FT88-Regular');
+	text(shared_time.gameTimer, 10, 575);
+	textSize(60);
 }
 
 // Defines drawn pixel
@@ -169,18 +186,23 @@ function drawPixels() {
 	// Defines width and length of each pixel based on 400
 	for (let row = 0; row < 5; row++) {
 		for (let col = 0; col < 5; col++) {
-			const x = col * 80;
-			const y = row * 80;
+			const x = col * 100;
+			const y = row * 100;
 
 			if (shared.grid[col][row]) {
 				const currentColor = shared.grid[col][row];
 				const currentImage = imgs[currentColor]
 				if (currentColor === 'tomato') {
-					image(testImg, x + 1, y + 1, 80 - 2, 80 - 2,)
+					image(lychee, x + 1, y + 1, 100 - 2, 100 - 2,)
 				
+				} else if (currentColor === 'darkorange') {
+					image(kumquat, x + 1, y + 1, 100 - 2, 100 - 2,)
+				
+				} else if (currentColor === 'chartreuse') {
+					image(durian, x + 1, y + 1, 100 - 2, 100 - 2,)
 				} else {
 					fill(color(currentColor));
-					rect(x + 1, y + 1, 80 - 2, 80 - 2, 2, 2, 2, 2);
+					rect(x + 1, y + 1, 100 - 2, 100 - 2, 2, 2, 2, 2);
 				}
 			}
 		}
@@ -202,18 +224,18 @@ function assignPlayers() {
 		const o = guests.find((p) => p.role === 'observer');
 		if (o === me) o.role = 'darkorange';
 	}
-	if (!guests.find((p) => p.role === 'gold')) {
-		const o = guests.find((p) => p.role === 'observer');
-		if (o === me) o.role = 'gold';
-	}
 	if (!guests.find((p) => p.role === 'chartreuse')) {
 		const o = guests.find((p) => p.role === 'observer');
 		if (o === me) o.role = 'chartreuse';
 	}
-	if (!guests.find((p) => p.role === 'aqua')) {
-		const o = guests.find((p) => p.role === 'observer');
-		if (o === me) o.role = 'aqua';
-	}
+	// if (!guests.find((p) => p.role === 'chartreuse')) {
+	// 	const o = guests.find((p) => p.role === 'observer');
+	// 	if (o === me) o.role = 'chartreuse';
+	// }
+	// if (!guests.find((p) => p.role === 'aqua')) {
+	// 	const o = guests.find((p) => p.role === 'observer');
+	// 	if (o === me) o.role = 'aqua';
+	// }
 }
 
 function gameTimer() {
@@ -281,8 +303,8 @@ function determineWinner() {
 }
 
 function displayResult(result) {
-	createCanvas(400, 500);
-	background('rgb(255, 0, 255)');
+	createCanvas(windowWidth, windowHeight);
+	background('lightpink');
 	//  Define in setup to fix the look of the timer
 	// fill("white");
 	textStyle(CENTER);
